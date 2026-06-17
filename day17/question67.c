de7189fca4dcfcbfd68aae00b940e89975d78e26
ritik raj <ritikbond.7@gmail.com>
2026-06-17 21:15:07 +0530

    day 17

---
+++ day17/question67.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], b[100], c[100];
+    int n1, n2, i, j = 0, k, found;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter elements of first array:");
+    for(i = 0; i < n1; i++) scanf("%d", &a[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter elements of second array:");
+    for(i = 0; i < n2; i++) scanf("%d", &b[i]);
+
+    for(i = 0; i < n1; i++) {
+        found = 0;
+        for(k = 0; k < n2; k++) {
+            if(a[i] == b[k]) {
+                found = 1;
+                break;
+            }
+        }
+        if(found) {
+            c[j++] = a[i];
+        }
+    }
+
+    printf("Intersection of arrays:");
+    for(i = 0; i < j; i++) printf("%d ", c[i]);
+
+    return 0;
+}
